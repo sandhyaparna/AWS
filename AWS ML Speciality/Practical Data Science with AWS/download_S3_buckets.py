30fd4e99fb01ac5b5eff8380392c5d611f344496
Sandhya Aparna Pashikanti <sandhyaparna@users.noreply.github.com>
2021-08-15 15:25:54 -0500

    Create download_S3_buckets.py

---
+++ AWS ML Speciality/Practical Data Science with AWS/download_S3_buckets.py
@@ -0,0 +1,15 @@
+# Download entire data in S3 bucket into SageMakeStudio
+# bucket path is s3://sagemaker-us-east-1-421851816171
+# a folder called s3_bucket_data will be created in sagemaker studio console
+!aws s3 sync s3://sagemaker-us-east-1-421851816171 s3_bucket_data  
+ 
+# Zip contents of the s3_bucket_data in SageMakeStudio console to a zipped folder and download the zipped folder
+# https://stackoverflow.com/questions/54931270/download-an-entire-folder-from-aws-sagemaker-to-laptop
+sudo yum install zip unzip
+zip -r -X archived_folder_name.zip . # to zip all contents in studio console
+zip -r -X archived_folder_name.zip s3_bucket_data  # to zip contents of a specific folder only. in this eg s3_bucket_data is zipped 
+
+
+
+
+
